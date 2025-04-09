696f39429700a857f6d7be1e478bd6d07e9c4465
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2025-04-09 15:45:44 +0200

    Added the backend funcitonality of fetching the pinned github repositories

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,28 @@
 import express, { Request, Response } from 'express';
+import { getPinnedRepositories } from "./github-interactions";
 
-const port = process.env.PORT || 80;
+if (process.env.NODE_ENV === "development") {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+}
+
 const app = express();
 
-app.get("/", (request: Request, responds: Response) => {
-  responds.json({ message: "backend operational" });
+app.get("/api/v1/github/pinned-repositories", async (request: Request, responds: Response) => {
+  try { 
+    const pinnedRepositories = await getPinnedRepositories()
+    responds.json(pinnedRepositories); 
+  } 
+
+  catch (err) {
+    console.error(err);
+    responds.status(500).json({ error: 'an unknown error occurred' });
+  }
 });
 
-app.listen(port, () => console.log(`Running on port ${port}`));
+
+const port = process.env.PORT || 80;
+app.listen(port, () => {
+  const mode = process.env.NODE_ENV || "an unknown";
+  const external_url = process.env.TVDH_BACKEND_SERVER_URL_EXTERNAL || "an unknown url.";
+  console.log(`Running on port ${port} in ${mode} mode. Available at ${external_url}`);
+});
